711ca3c885dcac5aff8ec02b0df38f048f50e14d
Nick Triozzi <triozzi.nick.88@gmail.com>
2025-02-25 15:15:39 +0100

    Initial Commit for JCAA article

---
+++ R/using.R
@@ -0,0 +1,10 @@
+# function to install multiple packages
+using<-function(...) {
+  libs<-unlist(list(...))
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  if(length(need)>0){ 
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
+  }
+}
